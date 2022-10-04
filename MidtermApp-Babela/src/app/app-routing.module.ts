c59761e23b6be6aaae7309c1b311e8dc64369a32
UBSITBabela <111262198+MielBabela@users.noreply.github.com>
2022-10-04 22:14:08 +0800

    Midterm APP
    10/4/22

--- MidtermApp-Babela/src/app/app-routing.module.ts
+++ MidtermApp-Babela/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ContactsInfoComponent } from './contacts-info/contacts-info.component';
+import { CourseInfoComponent } from './course-info/course-info.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    [
+    {path:'course-info', component: CourseInfoComponent},
+    {path:'course-list', component: CourseListComponent},
+    {path:'articles', component: ArticlesComponent},
+    {path:'add-course', component: AddCourseComponent},
+    {path:'contacts-info', component: ContactsInfoComponent }
+    ]
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- MidtermApp-Babela/src/app/app.module.ts
+++ MidtermApp-Babela/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './navi/navi.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseInfoComponent } from './course-info/course-info.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { ContactsInfoComponent } from './contacts-info/contacts-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    CourseListComponent,
+    CourseInfoComponent,
+    ArticlesComponent,
+    AddCourseComponent,
+    ContactsInfoComponent,
   ],
   imports: [
     BrowserModule,
